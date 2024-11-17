29a22c8c18e3a0660d974624596e9b299af24011
NaRkIsJaHaN <s92068909@ousl.lk>
2024-11-17 21:15:17 +0530

    Role management Page, grid, view role, add role, edit role, delete role

--- front-end/src/components/Background.tsx
+++ front-end/src/components/Background.tsx
@@ -10,7 +10,12 @@ import EditOrganization from './EditOrganization';
 import UserOverview from './UserOverview';
 import Users from './Users';
 import Template from './Template';
-import Category from './category';
+import Category from './Category';
+import Role from './Role';
+import AddRole from './AddRole';
+import ViewRole from './ViewRole';
+import EditRole from './EditRole';
+
 
 
 const Background: React.FC = () => {
@@ -25,9 +30,14 @@ const Background: React.FC = () => {
         <Route path="/users" element={<Users />} />
         <Route path='/templates' element={<Template/>}/>
         <Route path='/categories' element={<Category/>}/>
+        <Route path='/roles' element={<Role/>}/>
         <Route path="/create-organization" element={<CreateOrganization/>}/>
+        <Route path="/add-role" element={<AddRole/>}/>
         <Route path="/view-organization/:orgId" element={<ViewOrganization></ViewOrganization>} />
         <Route path="/Edit-organization/:orgId" element={<EditOrganization/>}/>
+        <Route path="/view-role/:roleId" element={<ViewRole/>}/>
+        <Route path="/edit-role/:roleId" element={<EditRole/>}/>
+
       </Routes>
     </Box>
   );
