e06e288d84abc9677028c04e31444b797dc52ee4
Charan Romi <charanromi325@gmail.com>
2024-11-20 17:37:15 +0530

    E2F1U4-1063: all APIs for form fields created

--- back-end/forms-management/src/form-fields/form-fields.service.ts
+++ back-end/forms-management/src/form-fields/form-fields.service.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { In } from 'typeorm';
 import { FormFieldsRepository } from '../form-fields/form-fields.repository';
 import { FormField } from '../model/form-fields.entity';
 import { CreateFormFieldDto } from '../form-fields/create-form-field.dto';
+import { UpdateFormFieldDto } from './update-form-field.dto';
 
 @Injectable()
 export class FormFieldsService {
@@ -28,5 +30,38 @@ export class FormFieldsService {
         }
         return field;
     }
+
+    // Method to delete a form field
+    async deleteField(fieldId: string): Promise<void> {
+        const field = await this.formFieldsRepository.findOne({ where: { fieldId } });
+        if (!field) {
+          throw new Error('Form Field not found');
+        }
+        field.status = 'deleted';
+        await this.formFieldsRepository.save(field);
+    }
+
+    // Method to bulk delete form fields
+    async bulkDeleteFields(fieldIds: string[]): Promise<void> {
+        const fields = await this.formFieldsRepository.find({ where: { fieldId: In(fieldIds) } });
+        if (fields.length === 0) {
+            throw new NotFoundException('No form fields found for the given IDs');
+          }
+        fields.forEach(field => {
+          field.status = 'deleted';
+        });
+        await this.formFieldsRepository.save(fields);
+    }
+
+    // Method to update a form field
+    async updateField(fieldId: string, updateFormFieldDto: UpdateFormFieldDto): Promise<FormField> {
+        const field = await this.formFieldsRepository.findOne({ where: { fieldId } });
+        if (!field) {
+          throw new Error('Form Field not found');
+        }
+        Object.assign(field, updateFormFieldDto);
+        return this.formFieldsRepository.save(field);
+    }
+       
 }
 
